d1f8c6da0b896afea30a58d4a86d8a70a5f51efc
darideveloper <darideveloper@gmail.com>
2024-03-11 15:07:16 -0600

    fix (scraper/get_business_data): social links

--- __main__.py
+++ __main__.py
@@ -1,23 +1,43 @@
 import os
 from time import sleep
-from libs.web_scraping import WebScraping
 from tqdm import tqdm
 from dotenv import load_dotenv
+from libs.web_scraping import WebScraping
+from libs.xlsx import SpreadsheetManager
 
+# env variables
 load_dotenv()
 CHROME_PATH = os.getenv("CHROME_PATH")
 
+# Paths
+CURRENT_FOLDER = os.path.dirname(__file__)
+EXCEL_PATH = os.path.join(CURRENT_FOLDER, "data.xlsx")
 
 class Scraper(WebScraping):
     
     def __init__(self):
-        """ Start chrome
+        """ Start chrome and open excel file
         """
         
+        # Start chrome
         self.home_page = "https://www.embassypages.com/spain"
         super().__init__(
             chrome_folder=CHROME_PATH,
         )        
+        
+        # Open excel path
+        self.sheet_name = "data"
+        self.ss_manager = SpreadsheetManager(EXCEL_PATH)
+        
+        # Delete old data sheet
+        try:
+            self.ss_manager.delete_sheet(self.sheet_name)
+        except Exception:
+            pass  
+        
+        # Set sheet
+        self.ss_manager.create_set_sheet(self.sheet_name)
+                
     
     def get_business_data(self) -> list:
         """ Scrape the business data
@@ -39,39 +59,37 @@ class Scraper(WebScraping):
         data = []
                 
         selectors = {
-            'address': '.address p',
-            'phone': '.telephone a',
-            'fax': '.fax p',
-            'emails': '.email a',
-            'socials': '.social__media',
+            "texts": {
+                'address': '.address p',
+                'phone': '.telephone a',
+                'fax': '.fax p',
+                'emails': '.email a',
+            },
+            "links": {
+                'socials': '.social__media a',
+            }
         }
         
-        for _, selector_value in selectors.items():
-            values = self.get_texts(selector_value)
-            values_clean = list(map(
-                lambda value: value.strip().replace("\n", ""), values
-            ))
-            value_formatted = ", ".join(values_clean)
-            data.append(value_formatted)
+        for selector_category, selectors_data in selectors.items():
+            for _, selector_value in selectors_data.items():
+                
+                # Get text or links
+                if selector_category == "texts":
+                    values = self.get_texts(selector_value)
+                else:
+                    values = self.get_attribs(selector_value, "href")
+                
+                # Clean and save value
+                values_clean = list(map(
+                    lambda value: value.strip().replace("\n", ""), values
+                ))
+                value_formatted = ", ".join(values_clean)
+                data.append(value_formatted)
             
         return data
     
-    def scrape_business(self) -> list:
-        """ Scrape all the business data
-
-        Returns:
-            list: list of business data
-            
-            Structure:
-            [
-                [
-                    "Address",
-                    "Phone",
-                    "Fax",
-                    "Emails",
-                    "Socials"
-                ]
-            ]
+    def scrape_business(self):
+        """ Scrape all the business data, and save in excel
         """
         
         # Show status
@@ -116,7 +134,10 @@ class Scraper(WebScraping):
             # Increase counter
             business_index += 1
             sleep(20)
-               
+            
+            # Write data in excel
+            self.ss_manager.write_data([data])
+            self.ss_manager.save()
 
 if __name__ == "__main__":
     scraper = Scraper()
